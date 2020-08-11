13f92198851cc56c40744305d4cef110ec331da1
Vishnu S. Penubarthi <35707033+vspenubarthi@users.noreply.github.com>
2020-08-11 12:03:52 -0400

    Initial Commit with Files

---
+++ QuickFinder/QuickFinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuickFinder
+//
+//  Created by Ruthvik Penubarthi on 8/11/20.
+//  Copyright © 2020 Vishnu Penubarthi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, There!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
